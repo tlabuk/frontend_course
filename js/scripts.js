504a14e6a65fa5b2a0e0e37903e61db1b31790d0
tlabuk <tomek.labuk@gmail.com>
2022-05-01 19:18:54 +0200

    add js script + table style

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var lp = 1;
+const Name = ['Anna', 'Katarzyna', 'Marek', 'Joanna', 'Mariusz', 'Longin', 'Dobromir']
+const Surname = ['Kowalska', 'Pierchowska', 'Miedrzycka', 'Bobicka', 'Mariuszowski', 'Podbipieta', 'Bobkowski']
+
+function tableCreate() {
+    var tbody = document.getElementsByTagName("tbody")[0];
+    for (var i = 0; i < Name.length; i++) {
+        
+        var row = document.createElement("tr");
+        for (var j = 0; j < 1; j++) {
+
+            var cell = document.createElement("td"); 
+            var cellText = document.createTextNode(`${[i + 1]}`);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+        for (var j = 1; j < 2; j++) {
+
+        var cell = document.createElement("td");   
+        var cellText = document.createTextNode(`${Name[i]}`);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        for (var j = 2; j < 3; j++) {
+
+            var cell = document.createElement("td");   
+            var cellText = document.createTextNode(`${Surname[i]}`);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+        
+        tbody.appendChild(row);
+        continue;
+    }
+
+    row.appendChild(cell);
+    
+}
+
+tableCreate();
